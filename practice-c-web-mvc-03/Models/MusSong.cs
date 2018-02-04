5e465d33dc5b22fbf6a72fecd612714a3952c7ca
carlrdj <carl.rdj@gmail.com>
2018-02-04 13:01:21 -0500

    C# using Scaffolding .net

--- practice-c-web-mvc-03/Models/MusSong.cs
+++ practice-c-web-mvc-03/Models/MusSong.cs
@@ -10,6 +10,8 @@ namespace practice_c_web_mvc_03.Models
         public int Id { get; set; }
         public int MusArtistId { get; set; }
         public string Name { get; set; }
+        //[Required]
+        //[MaxLenght(10)]
         public float Time { get; set; }
         public int Status { get; set; }
         public virtual MusArtist Artist { get; set; }
